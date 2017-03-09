0fdae4633bb34ee4a4187f976fa3a84cb0d6b287
sergiogorge <sergio1345@gmail.com>
2017-03-09 10:24:04 +0100

    Delete conexionbd2.php

--- proyecto/conexionbd2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$connection = new mysqli("localhost", "root", "2asirtriana", "proyecto_blog2");
-
-if ($connection->connect_errno) {
-    printf("Connection failed: %s\n", $connection->connect_error);
-    exit();
-  }
-    ?>
